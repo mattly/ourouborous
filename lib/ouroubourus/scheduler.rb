acea7c8e11613a625ae901229364c6c5a91adc3c
Matt Lyon <matt@flowerpowered.com>
2008-10-30 23:38:46 -0700

    set the timekeeper on init for Scheduler

--- lib/ouroubourus/scheduler.rb
+++ lib/ouroubourus/scheduler.rb
@@ -5,14 +5,13 @@ module Ouroubourus
     
     def initialize(options={})
       @subscribers = []
+      @timekeeper = options[:timekeeper]
     end
     
     def start
       return false unless timekeeper.kind_of?(Ouroubourus::Timekeeper)
       timekeeper.start {|now| @subscribers.each{|s| Thread.start { s.run(now) } }}
     end
-    
-    
-    
+
   end
 end
