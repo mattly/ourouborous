614b1b79c9e17322a6af7ee75b3f5a5f08c63663
Matt Lyon <matt@flowerpowered.com>
2008-11-01 11:19:43 -0700

    now with more random

--- examples/beats.rb
+++ examples/beats.rb
@@ -16,8 +16,8 @@ Debugger.start
 @midi.interface.program_change 0, 115 # wood block
 @midi.interface.driver.instance_variable_set(:@destination, MIDIator::Driver::CoreMIDI::C.mIDIGetDestination(1)) # ugly hack for now
 @beats = StepSequencer.new :interface => @midi
-@beats.sequences << StepSequencer::Sequence.new(60, :sequence => %w(1 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0))
-@beats.sequences << StepSequencer::Sequence.new(62, :sequence => %w(0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0))
+@beats.sequences << StepSequencer::Sequence.new(60, :sequence => %w(1 0 0 0.25 0 0 0 0    1 0 0.75 0 0 0.9 0   0.5))
+@beats.sequences << StepSequencer::Sequence.new(62, :sequence => %w(0 0 0 0    1 0 0 0.33 0 0 0    0 1 0   0.5 0.5))
 @s.subscribers << @beats
 
 Signal.trap("INT") { @s.timekeeper.thread.exit!; "Interrupt caught, cancelling..." }
