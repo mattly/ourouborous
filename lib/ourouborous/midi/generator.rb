35316778de9c49fada99a094ca388af6519457e9
Matt Lyon <matt@flowerpowered.com>
2008-11-05 16:22:41 -0800

    generator keeps track of noteons and when they started

--- lib/ourouborous/midi/generator.rb
+++ lib/ourouborous/midi/generator.rb
@@ -2,6 +2,12 @@ module Ourouborous
   module MIDI
     module Generator
       
+      attr_accessor :note_states
+      
+      def initialize(*a, &b)
+        @note_states = Hash.new {|h, key| h[key] = nil }
+      end
+      
       def play(notes)
         [notes].flatten.each do |note|
           if note.start.zero?
@@ -15,11 +21,27 @@ module Ourouborous
       end
       
       def note_on(channel, pitch, velocity)
-        @interface.note(channel, pitch, velocity)
+        if velocity.zero? ? playing?(channel, pitch) : ! playing?(channel, pitch)
+          record_note(channel, pitch, velocity)
+          @interface.note(channel, pitch, velocity)
+        end
       end
       
       def note_off(channel, pitch)
         note_on(channel, pitch, 0)
+      end
+      
+      def playing?(channel, pitch)
+        ! @note_states["#{channel}x#{pitch}"].nil?
+      end
+      
+    protected
+      def record_note(channel, pitch, velocity)
+        note = {:velocity => velocity}
+        if @schedule && @schedule.time
+          note.merge!({:started => @schedule.time})
+        end
+        @note_states["#{channel}x#{pitch}"] = note
       end
       
     end
