42d3d4d4462a447f458db24e25294d0e4c3495a8
Matt Lyon <matt@flowerpowered.com>
2008-11-05 20:13:23 -0500

    midiator autodetects drivers, gets program change

--- lib/interfaces/midiator.rb
+++ lib/interfaces/midiator.rb
@@ -6,10 +6,15 @@ class Midiator
   
   def initialize
     @interface = MIDIator::Interface.new
+    @interface.autodetect_driver
   end
   
   def note(channel, pitch, velocity)
     @interface.driver.note_on(pitch, channel, velocity)
+  end
+  
+  def program_change(channel, program)
+    @interface.program_change channel, program
   end
   
 end
